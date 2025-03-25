c50ff535d667df6baf6d6867cd5f0f6a68bea2be
diegosouza25 <diegoalunocetep25@gmail.com>
2025-03-25 19:10:08 +0000

    criei o arquivo variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,7 @@
+var nome = "diego" //variavel que muda o valor na execução do codigo 
+let idade = 17 //  Variavel que muda o valor na execução do codigo 
+const signo = "escopião" // Varisvel contante não muda de valor durante a execição 
+/* variavel que muda o valor na execução do codigo 
+ variavel que muda o valor na execução do codigo
+ variavel constante não muda de valor durante a execução
+ */
